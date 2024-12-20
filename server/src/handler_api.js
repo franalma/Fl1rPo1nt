605929fdd4ef5ea63775e8d857f12b61a850d5c0
FranAlma <franalmeida@gmail.com>
2024-12-20 07:09:34 +0100

    chnages

--- server/src/handler_api.js
+++ server/src/handler_api.js
@@ -1,29 +1,25 @@
-const serverless = require('serverless-http');
-const apiServer = require('./server_api');
+const serverless = require("serverless-http");
+const apiServer = require("./server_api");
 const logger = require("./logger/log");
 const dbHandler = require("./database/database_handler");
 const { DB_INSTANCES } = require("./database/databases");
-const { printJson } = require('./utils/json_utils');
-
-
+const { printJson } = require("./utils/json_utils");
 
 exports.handler = async (event, context) => {
-    try {
-        await dbHandler.connectToDatabase(DB_INSTANCES.DB_API);
-        const serverlessHandler = serverless(apiServer.app);
-        const response = await serverlessHandler(event, context);
-        const result = {
-            statusCode: 200,
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: response.body
-        };
-        printJson(JSON.parse(result));
-        return result; 
+  try {
+    await dbHandler.connectToDatabase(DB_INSTANCES.DB_API);
+    const serverlessHandler = serverless(apiServer.app);
+    const response = await serverlessHandler(event, context);
+    const result = {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json", // Set Content-Type
+      },
+      body: JSON.stringify(response.body),
+    };
 
-    } catch (error) {
-        logger.info(error);
-    }
-
-}
+    return result;
+  } catch (error) {
+    logger.info(error);
+  }
+};
