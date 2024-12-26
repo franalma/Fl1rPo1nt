3481325150b02921619e8af3927fa7682d1d9d15
Fran Almeida <franalmeida@gmail.com>
2024-12-26 21:05:25 +0100

    changes

--- aws/main.tf
+++ aws/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 # VPC
 resource "aws_vpc" "vpc_floiint_t" {
 
-  cidr_block           = "172.31.0.0/24"
+  cidr_block           = "172.31.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
